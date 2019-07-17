44262213f61b20a43e9bbeffd9e61f2c64b55d36
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-17 12:32:25 +0200

    All constructor was chanded (this.<field> = <method_paramiter> )

--- src/pl/homework/lib/Point.java
+++ src/pl/homework/lib/Point.java
@@ -8,8 +8,8 @@ public class Point {
     }
 
     public Point(double x, double y) {
-        setX(x);
-        setY(y);
+        this.x = x;
+        this.y = y;
     }
 
     public double getX() {
